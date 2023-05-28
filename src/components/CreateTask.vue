5de46d9a2a810da3c01712d39ca2423d8db99d03
gsquevedo <gsquevedo@inf.ufsm.br>
2023-05-28 19:34:27 -0300

    testes

--- src/components/CreateTask.vue
+++ src/components/CreateTask.vue
@@ -61,10 +61,10 @@ export default {
             user
         }
     },
-    async created() {
+    created() {
         // let apiURL = `http://localhost:4000/api/${this.$route.params.id}`;
         let apiURL = `https://project3-taskmentor-api.vercel.app/api/${this.$route.params.id}`
-        await axios.get(apiURL).then((res) => {
+        axios.get(apiURL).then((res) => {
             this.user = res.data;
         })
     },
